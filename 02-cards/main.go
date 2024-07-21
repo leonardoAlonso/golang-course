8f16efbcc5a606bd1c213a8f3f4d09e17034ac44
Leonardo <leonardo.alonso@benandfrank.com>
2024-07-20 21:08:03 -0600

    feat: write on hard drive

--- 02-cards/main.go
+++ 02-cards/main.go
@@ -7,5 +7,5 @@ func main() {
 	hand, remainingDeck := deal(cards, 5)
 	fmt.Println("Hand: ", hand)
 	fmt.Println("Remaining Deck : ", remainingDeck)
-	fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
