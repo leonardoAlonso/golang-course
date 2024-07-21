40bb536c9682c0d4842065f3ce35aafa14c8fe4d
Leonardo <leonardo.alonso@benandfrank.com>
2024-07-20 21:00:04 -0600

    feat: type convertion

--- 02-cards/main.go
+++ 02-cards/main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	cards := newDeck()
-	// cards.print()
 	hand, remainingDeck := deal(cards, 5)
 	fmt.Println("Hand: ", hand)
 	fmt.Println("Remaining Deck : ", remainingDeck)
+	fmt.Println(cards.toString())
 }
