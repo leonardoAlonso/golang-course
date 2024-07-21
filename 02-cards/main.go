d6224a98cf6ec373a0eea201ef68f2cd417ea0d0
Leonardo <leonardo.alonso@benandfrank.com>
2024-07-20 20:57:48 -0600

    feat: multiple return values

--- 02-cards/main.go
+++ 02-cards/main.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
 
 func main() {
 	cards := newDeck()
-  cards.print()
+	// cards.print()
+	hand, remainingDeck := deal(cards, 5)
+	fmt.Println("Hand: ", hand)
+	fmt.Println("Remaining Deck : ", remainingDeck)
 }
-
