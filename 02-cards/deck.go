dc71c100f7a15906802b5ce71f7f0a51348a7b72
Leonardo <leonardo.alonso@benandfrank.com>
2024-07-22 09:11:12 -0600

    feat: read from file

--- 02-cards/deck.go
+++ 02-cards/deck.go
@@ -41,3 +41,13 @@ func (d deck) toString() string {
 func (d deck) saveToFile(fileName string) error {
 	return os.WriteFile(fileName, []byte(d.toString()), 0666)
 }
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
--- 02-cards/main.go
+++ 02-cards/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
+	cards := newDeckFromFile("my_cards")
 	hand, remainingDeck := deal(cards, 5)
 	fmt.Println("Hand: ", hand)
 	fmt.Println("Remaining Deck : ", remainingDeck)
-	cards.saveToFile("my_cards")
+	// cards.saveToFile("my_cards")
 }
