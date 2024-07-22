16e4b1c5f69fa2a5e348394ec1b6deed735a2689
Leonardo <leonardo.alonso@benandfrank.com>
2024-07-22 09:33:24 -0600

    feat: random number generation

--- 02-cards/main.go
+++ 02-cards/main.go
@@ -1,14 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	// cards := newDeck()
-	cards := newDeckFromFile("my_cards")
-	hand, remainingDeck := deal(cards, 5)
-	fmt.Println("Hand: ", hand)
-	fmt.Println("Remaining Deck : ", remainingDeck)
+	cards := newDeck()
+	// cards := newDeckFromFile("my_cards")
+	// hand, remainingDeck := deal(cards, 5)
+	// fmt.Println("Hand: ", hand)
+	// fmt.Println("Remaining Deck : ", remainingDeck)
 	// cards.saveToFile("my_cards")
+	cards.shuffle()
+	cards.print()
 }
