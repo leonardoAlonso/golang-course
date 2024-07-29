707ace6b6dd1453435357fc257588079c3ee0935
Leonardo <leonardo.alonso@benandfrank.com>
2024-07-29 09:11:30 -0600

    feat: embebedding structs

--- 05-structs/main.go
+++ 05-structs/main.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+  email string
+  zipCode int
+}
+
 type person struct {
   firstName string
   lastName string
+  contact contactInfo
 }
 
 func main() {
-
-  var alex person
-  alex.firstName = "Alex"
-  fmt.Printf("%+v", alex)
+  jim := person{
+    firstName: "Jim",
+    lastName: "Party",
+    contact: contactInfo{
+      email: "jim@mail.com",
+      zipCode: 94000,
+    },
+  }
+  fmt.Printf("%+v", jim)
 }
+
